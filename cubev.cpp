6d416beb1794ab493b1a4a41abcf761961d1d130
satpalbeniwal8558-source <satpalbeniwal8558@gmail.com>
2026-01-29 17:50:38 +0530

    Add files via upload

---
+++ cubev.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float l,v;
+    cout << "enter the value of l ";
+    cin >> l;
+
+    v=l*l*l;
+
+    cout << "volume of cube = " << v << endl;
+
+    return 0;
+}
