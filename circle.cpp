98aba08160f191aeb633f751fe0891c6d6effb9a
satpalbeniwal8558-source <satpalbeniwal8558@gmail.com>
2026-01-29 17:49:50 +0530

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float r,a;
+    cout << "enter the value of r ";
+    cin >> r;
+
+    a=3.14*r*r;
+
+    cout << "area of circle = " << a << endl;
+
+    return 0;
+}
